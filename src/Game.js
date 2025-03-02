734121a5d969963f116ee91862bdaf461246bd64
rami2076 <sakusennx@gmail.com>
2025-03-02 18:09:51 +0900

    AIの追加

--- src/Game.js
+++ src/Game.js
@@ -24,6 +24,18 @@ export const TicTacToe = {
       return { draw: true };
     }
   },
+
+  ai: {
+    enumerate: (G, ctx) => {
+      let moves = [];
+      for (let i = 0; i < 9; i++) {
+        if (G.cells[i] === null) {
+          moves.push({ move: "clickCell", args: [i] });
+        }
+      }
+      return moves;
+    },
+  },
 };
 
 // Return true if `cells` is in a winning configuration.
